d32a7275d2cc63c6ff32f78036b95756e2484bb7
Aaron Russo <aaronrusso@hotmail.it>
2018-01-11 18:41:27 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+from setuptools import setup, find_packages
 from distutils.core import setup
+
 setup(
-  name='ubm',
-  packages=['ubm'],  # this must be the same as the name above
-  version='v1.0.0',
-  description='Library for UniBo Motorsport data analysis',
-  author='Aaron Russo',
-  author_email='axolo6@gmail.com',
-  url='https://github.com/Ax6/ubm-python-libraries',  # use the URL to the github repo
-  download_url='https://github.com/Ax6/ubm-python-libraries/archive/v1.0.0.tar.gz',
-  classifiers=[],
+    name='ubm',
+    packages=find_packages(),
+    version='v1.0.0',
+    description='Library for UniBo Motorsport data analysis',
+    author='Aaron Russo',
+    author_email='axolo6@gmail.com',
+    url='https://github.com/Ax6/ubm-python-libraries',  # use the URL to the github repo
+    download_url='https://github.com/Ax6/ubm-python-libraries/archive/v1.0.0.tar.gz',
+    classifiers=[],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas'
+    ],
 )
