d0bbfef1fbf0aa643de27f5b07aff434e6d14087
Aaron Russo <aaronrusso@hotmail.it>
2018-01-11 17:35:38 +0100

    Updated to tag version and added setup cfg

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
   name='ubm',
   packages=['ubm'],  # this must be the same as the name above
-  version='0.1',
+  version='v1.0.0',
   description='Library for UniBo Motorsport data analysis',
   author='Aaron Russo',
   author_email='axolo6@gmail.com',
   url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
-  download_url='https://github.com/Ax6/ubm-python-libraries/archive/0.1.tar.gz',
+  download_url='https://github.com/Ax6/ubm-python-libraries/archive/v1.0.0.tar.gz',
   classifiers=[],
 )
